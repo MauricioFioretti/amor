5d5dac588fbeade04a8117726270a2fab758f8d7
Mauricio Fioretti <mauriciofioretti@gmail.com>
2025-12-28 13:33:57 -0300

    arreglando pag admin 2

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-const API_LIST = "Thttps://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg5BCWRDxHj39nKbSdLQpxgXe3v1jkfpB1y2LvhfTWnz0FsLSVJSUP75vom3aE-c7DMhHF7Ej1hdynpx69lkr4cjpQVjVygUji36ofWkNhH-90QTA6CzBl5yU--gXoEZDl-NwSydMAVs_tDlMxtALrn54ATbLqmE6qZ7GkXc6lWH82BrD58L-DgFOmS3SfqgI3RPYdwbrCIm1OrizM9jWgBzhho5nwONyQOcNO8r6s0KGRKj_kFgvgyV4J_utyOfBme04Y0JwYYLMfuAPwJ8jFROzfLAA&lib=M4JruGeuSbuyEj-RXPzQaUaaZ4aZnzWxY"
+const API_LIST = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLg5BCWRDxHj39nKbSdLQpxgXe3v1jkfpB1y2LvhfTWnz0FsLSVJSUP75vom3aE-c7DMhHF7Ej1hdynpx69lkr4cjpQVjVygUji36ofWkNhH-90QTA6CzBl5yU--gXoEZDl-NwSydMAVs_tDlMxtALrn54ATbLqmE6qZ7GkXc6lWH82BrD58L-DgFOmS3SfqgI3RPYdwbrCIm1OrizM9jWgBzhho5nwONyQOcNO8r6s0KGRKj_kFgvgyV4J_utyOfBme04Y0JwYYLMfuAPwJ8jFROzfLAA&lib=M4JruGeuSbuyEj-RXPzQaUaaZ4aZnzWxY"
 const API_EXEC = "https://script.google.com/macros/s/AKfycbxR533VEsnctfiJ5qmb1H3srZX3LMRT3oWCUsLKaNP5pkXXQ-HU6ufU2AQULk5a3LQM/exec"
 
 // ================== HELPERS ==================
